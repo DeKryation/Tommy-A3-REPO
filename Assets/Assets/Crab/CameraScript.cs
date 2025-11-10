a5dc8941db0f6419ff7ef6d1662f1775da31e75f
dindinren <joycelynb21@gmail.com>
2025-11-10 18:22:59 +0800

    Implemented Popup
    + Once near an interactable object, it will have a popup for the player to press

--- Assets/Assets/Crab/CameraScript.cs
+++ Assets/Assets/Crab/CameraScript.cs
@@ -22,7 +22,10 @@ public class CameraScript : MonoBehaviour
         mouseDelta = Mouse.current.delta.ReadValue();
         float newY = mouseDelta.y * -1;
         Vector3 rotation = new Vector3(newY * rotSpd, mouseDelta.x * rotSpd , 0);
-        transform.eulerAngles += rotation; 
+        transform.eulerAngles += rotation;
+
+        ///Raycast interaction - joycelyn
+        CheckInteraction();
     }
 
     public void UnlockCursor()
@@ -31,4 +34,28 @@ public class CameraScript : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
+
+    public TestInteract test;
+    public LayerMask mask;
+    public GameObject popup;
+    ///trying to implement Raycast Interaction here -Joycelyn
+    void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+
+        //if hit, show the interact button and play particle
+        if(Physics.Raycast(ray, out hit, 30, mask))
+        {
+            popup.SetActive(true);
+            test.DoOnRaycastHit();
+            
+        }
+        else
+        {
+            popup.SetActive(false);
+        }
+    }
+
+   
 }
